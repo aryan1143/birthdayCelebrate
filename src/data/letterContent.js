987ef51fa973aeccaa2189f00ccabe0adba4b6a9
Aryan <aryanshasap@gmail.com>
2026-07-04 17:33:26 +0530

    fix

--- src/data/letterContent.js
+++ src/data/letterContent.js
@@ -1,12 +1,12 @@
 export const LETTER_CONTENT = {
   greeting: "Dear Friend,",
   lines: [
-    "I hope you enjoyed the little journey today.",
-    "From solving the puzzle box...",
+    "I hope you enjoyed the little journey I prepared for youuuuu.",
+    "From solving the puzzle...",
     "To helping Rimuru find all the ingredients...",
-    "And even the tiny stumble while baking the cake.",
-    "Every step was made just to bring a smile to your face.",
+    "Every step was made just to bring a smile on your face.",
     "Thank you for being such an amazing person.",
-    "Happy Birthday. 🌸",
+    "Happy Birthday Koaibiettolaai. 🌸",
+    "From Shadow :3",
   ],
 };
